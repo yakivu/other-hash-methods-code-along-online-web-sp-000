f6512e967d6d27d8e057dc011f60e5d7504e2e41
Adam Walker <adamw207@yahoo.com>
2021-02-09 16:28:33 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,3 +1,17 @@
+#require 'pry'
+
+#This is the groceries hash we'll be passing in to the get_the_keys method
+# groceries = {
+#  dairy: ["milk", "yogurt", "cheese"],
+#  vegetable: ["carrots", "broccoli", "cucumbers"],
+#  meat: ["chicken", "steak", "salmon"],
+#  grains: ["rice", "pasta"]
+# }
+
+#def get_the_keys(groceries)
+  #code your solution here!
+#end
+
 require 'pry'
 
 #This is the groceries hash we'll be passing in to the get_the_keys method
@@ -9,5 +23,5 @@ require 'pry'
 # }
 
 def get_the_keys(groceries)
-  #code your solution here!
+  groceries.keys
 end
